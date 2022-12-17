cf558d5556c926a9ad0d9da4c5c4f94a4e2bd8f9
qismat <qismatnisha6243@gmail.com>
2022-12-18 01:52:33 +0530

    [qismat] Add: UC3-PowerOfTwoNumber

--- FuctionalProgrammingPrblms/FuctionalProgrammingPrblms/Program.cs
+++ FuctionalProgrammingPrblms/FuctionalProgrammingPrblms/Program.cs
@@ -6,7 +6,7 @@
         {
             while (true)
             {
-                Console.WriteLine("Select program \n1.Flip Coin \n2.Leap Year");
+                Console.WriteLine("Select program \n1.Flip Coin \n2.Leap Year\n3. PowerOfTwoNumber");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -17,6 +17,10 @@
                     case 2:
                         LeapYear leapYear = new LeapYear();
                         leapYear.yearcheck();
+                        break;
+                    case 3:
+                        PowerOfTwoNum powerOfTwoNum = new PowerOfTwoNum();
+                        powerOfTwoNum.Read();
                         break;
                 }
             }
