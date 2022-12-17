e0c406ef7073e155a5228cfcbbec84225be09a7b
qismat <qismatnisha6243@gmail.com>
2022-12-18 02:10:17 +0530

    [qismat] Add: UC6-QuotientRemainder

---
+++ FuctionalProgrammingPrblms/FuctionalProgrammingPrblms/QuotientRemainder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuctionalProgrammingPrblms
+{
+
+    public class QuotientRemainder
+    {
+        int dividend = 19, divisor = 5;
+        public void QuoRem()
+        {
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+            Console.WriteLine("quotient = " + quotient);
+            Console.WriteLine("remainder = " + remainder);
+        }
+    }
+}
