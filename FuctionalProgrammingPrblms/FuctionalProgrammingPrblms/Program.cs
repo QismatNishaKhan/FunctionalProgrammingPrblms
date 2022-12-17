b47de32068f3858b48a3706c6ab6204d191af22b
qismat <qismatnisha6243@gmail.com>
2022-12-18 02:23:10 +0530

    [qismat] Add: UC7-SwapTwoNumber

--- FuctionalProgrammingPrblms/FuctionalProgrammingPrblms/Program.cs
+++ FuctionalProgrammingPrblms/FuctionalProgrammingPrblms/Program.cs
@@ -4,10 +4,9 @@
     {
         static void Main(string[] args)
         {
-
             while (true)
             {
-                Console.WriteLine("Select program \n1.Flip Coin \n2.Leap Year\n3.Power of Two numbers\n4.Harmonic Number\n5.Factors of Number \n6.QuotientRemainder");
+                Console.WriteLine("Select program \n1.Flip Coin \n2.Leap Year\n3.Power of Two numbers\n4.Harmonic Number\n5.Factors of Number \n6.QuotientRemainder \n7.Swap Two Numbers \n8. check Given Number is Even or Odd \n9. Check an Alphabet is Vowel or Consonant");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -35,6 +34,19 @@
                         QuotientRemainder quotientRemainder = new QuotientRemainder();
                         quotientRemainder.QuoRem();
                         break;
+                    case 7:
+                        SwapTwoNumber swapTwoNumber = new SwapTwoNumber();
+                        swapTwoNumber.Swap();
+                        break;
+                    case 8:
+                        EvenOrOdd evenOrOdd = new EvenOrOdd();
+                        evenOrOdd.EvenandOdd();
+                        break;
+                    case 9:
+                        AlphabateIsVowelOrConsonant alphabateIsVowelOrConsonant = new AlphabateIsVowelOrConsonant();
+                        alphabateIsVowelOrConsonant.Alphabet();
+                        break;
+
                 }
             }
         }
